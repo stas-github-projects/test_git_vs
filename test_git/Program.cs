1ab89b4f38974554248304510d8f51fc4b655c45
stas-github-projects <thatsmytempmail@gmail.com>
2016-02-11 23:38:02 +0300

    https://github.com/stas-github-projects/test_git_vs

---
+++ test_git/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hj");
+            Console.ReadKey();
+        }
+    }
+}
